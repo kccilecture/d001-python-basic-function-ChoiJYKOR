bd7781eeec5ce58e054bfa1e267dc3b0e68f833a
Eric <juyeong.choi@gmail.com>
2026-01-07 12:13:31 +0900

    Before try the last def greet. this is git push test.

--- basic_func/basic_func.py
+++ basic_func/basic_func.py
@@ -1,26 +1,33 @@
 def add(a, b):
+    return a + b
     pass
 
 
 def sub(a, b):
+    return a - b
     pass
 
 
 def mul(a, b):
+    return a * b
     pass
 
 
 def div(a, b):
+    return a // b
     pass
 
 
 def power(base, pow):
+    return base**pow
     pass
 
 
 def square(base):
+    return base**2
     pass
 
 
 def greet(이름="낯선자", 나이=20):
+    return
     pass
